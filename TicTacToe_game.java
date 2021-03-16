5cce89d92d3cb7e7ecfe55b5cde6036a3f8c4cbe
vishnu <vishnu388@gmail.com>
2021-03-16 18:18:53 +0530

    [vishnu]: Added to player select

--- TicTacToe_game.java
+++ TicTacToe_game.java
@@ -2,19 +2,36 @@ package TicTacToe;
 
 public class TicTacToe {
 
-		//Method to initialize array elements
-	    public static void createBorad() {
-	            char [] board = new char[10];
-	            for (int index = 1; index < board.length; index++){
-	                board[index] = ' ';
-	            }
-	    }
+		 static char[] board = new char[10];
+		 public static char player;
+		 public static char computer;
 
-	    //main method to execute the program
-	    public static void main(String [] args){
-	        System.out.println("Welcome to Tic Tac Toe Game.");
-	        //calling method to create board
-	        createBorad();
+	    public static void createboard() {
+	        for (int i = 1; i < board.length; i++) {
+	            board[i] = ' ';
+	        }
 	    }
+ public static void playerSelect() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Symbol you choose to with (x/o): ");
+        String choice = scan.next();
+        switch(choice) {
+            case "X":
+                player = 'x';
+                computer = 'o';
+                break;
+            case "O":
+                player = 'o';
+                computer = 'x';
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
+    }
 
+	    public static void main(String[] args){
+	        createboard();
+			  playerSelect();
+	    }
 	}
